a96b01daf8c1b2f3c6a0bdb6089e995ecb476885
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-01-31 20:34:49 +0530

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function App() {
+	return <div className="App">ToDO App</div>;
+}
+
+export default App;
