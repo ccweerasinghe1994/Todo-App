504aba3e54e4abc272792653456cb5a8654d4e42
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-02-07 18:02:53 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-	return <div className="App">ToDO 12 App Update 2</div>;
+	return <div className="App">ToDO 122 App Update 2</div>;
 }
 
 export default App;
